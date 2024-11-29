10782fd22f05824fb55f4dc5db886892d13e026f
Phil Tsaryk <phil@tsaryk.com>
2024-11-29 23:36:30 +0100

    feat: send email on high level

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,74 @@ export interface Config {
   db: {
     uri: string
   }
+  email: {
+    host: string
+    port: number
+    user: string
+    pass: string
+    from: string
+    to: string
+    subject: string
+    text: string
+  }
+  warningLevel: number
 }
 
 export function getConfig(): Config {
-  const {DB_URI} = process.env
+  const {
+    DB_URI,
+    EMAIL_HOST,
+    EMAIL_PORT,
+    EMAIL_USER,
+    EMAIL_PASS,
+    EMAIL_FROM,
+    EMAIL_TO,
+    EMAIL_SUBJECT,
+    EMAIL_TEXT,
+    WARNING_LEVEL,
+  } = process.env
   if (!DB_URI) {
     throw new Error('DB_URI env variable is missing')
   }
-  return {db: {uri: DB_URI}}
+  if (!EMAIL_HOST) {
+    throw new Error('EMAIL_HOST env variable is missing')
+  }
+  if (!EMAIL_PORT) {
+    throw new Error('EMAIL_PORT env variable is missing')
+  }
+  if (!EMAIL_USER) {
+    throw new Error('EMAIL_USER env variable is missing')
+  }
+  if (!EMAIL_PASS) {
+    throw new Error('EMAIL_PASS env variable is missing')
+  }
+  if (!EMAIL_FROM) {
+    throw new Error('EMAIL_FROM env variable is missing')
+  }
+  if (!EMAIL_TO) {
+    throw new Error('EMAIL_TO env variable is missing')
+  }
+  if (!EMAIL_SUBJECT) {
+    throw new Error('EMAIL_SUBJECT env variable is missing')
+  }
+  if (!EMAIL_TEXT) {
+    throw new Error('EMAIL_TEXT env variable is missing')
+  }
+  if (!WARNING_LEVEL) {
+    throw new Error('WARNING_LEVEL env variable is missing')
+  }
+  return {
+    db: {uri: DB_URI},
+    email: {
+      host: EMAIL_HOST,
+      port: parseInt(EMAIL_PORT),
+      user: EMAIL_USER,
+      pass: EMAIL_PASS,
+      from: EMAIL_FROM,
+      to: EMAIL_TO,
+      subject: EMAIL_SUBJECT,
+      text: EMAIL_TEXT,
+    },
+    warningLevel: parseInt(WARNING_LEVEL),
+  }
 }
--- src/routes/level/index.ts
+++ src/routes/level/index.ts
@@ -1,22 +1,28 @@
 import {Route} from '..'
 import {exec, Level} from '../../db'
+import {email} from './email'
 
-export const level: Route = () => async (req, res) => {
-  const newValue = parseInt(req.query.value as string)
-  if (isNaN(newValue)) {
-    res.sendStatus(400)
-    return
-  }
-  await exec<Level>(
-    'levels',
-    async (collection) => {
-      await collection.insertOne({value: newValue, when: new Date()})
-      res.send({ok: true})
-    },
-    {
-      onError: () => {
-        res.sendStatus(503)
-      },
+export const level: Route =
+  ({config}) =>
+  async (req, res) => {
+    const newValue = parseInt(req.query.value as string)
+    if (isNaN(newValue)) {
+      res.sendStatus(400)
+      return
     }
-  )
-}
+    await exec<Level>(
+      'levels',
+      async (collection) => {
+        await collection.insertOne({value: newValue, when: new Date()})
+        res.send({ok: true})
+      },
+      {
+        onError: () => {
+          res.sendStatus(503)
+        },
+      }
+    )
+    if (newValue >= config.warningLevel) {
+      email({config: config.email, level: newValue})
+    }
+  }
