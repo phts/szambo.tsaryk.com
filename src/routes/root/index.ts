51f51bf50bc24202651f2e6dddd33a9b2dbe3387
Phil Tsaryk <phil@tsaryk.com>
2025-04-23 17:30:02 +0200

    feat(root): colorful level entries

--- src/routes/root/index.ts
+++ src/routes/root/index.ts
@@ -2,32 +2,34 @@ import {Route} from '..'
 import {exec, Level, Log} from '../../db'
 import {page} from './page'
 
-export const root: Route = () => async (req, res) => {
-  const pagedata: {levels: Level[]; logs: Log[]} = {levels: [], logs: []}
-  await exec<Level>(
-    'levels',
-    async (collection) => {
-      const cursor = collection.find().sort({when: -1})
-      pagedata.levels = await cursor.toArray()
-    },
-    {
-      onError: () => {
-        res.sendStatus(503)
+export const root: Route =
+  ({config}) =>
+  async (req, res) => {
+    const pagedata: {levels: Level[]; logs: Log[]} = {levels: [], logs: []}
+    await exec<Level>(
+      'levels',
+      async (collection) => {
+        const cursor = collection.find().sort({when: -1})
+        pagedata.levels = await cursor.toArray()
       },
-    }
-  )
-  await exec<Log>(
-    'logs',
-    async (collection) => {
-      const cursor = collection.find().sort({when: -1})
-      pagedata.logs = await cursor.toArray()
-    },
-    {
-      onError: () => {
-        res.sendStatus(503)
+      {
+        onError: () => {
+          res.sendStatus(503)
+        },
+      }
+    )
+    await exec<Log>(
+      'logs',
+      async (collection) => {
+        const cursor = collection.find().sort({when: -1})
+        pagedata.logs = await cursor.toArray()
       },
-    }
-  )
+      {
+        onError: () => {
+          res.sendStatus(503)
+        },
+      }
+    )
 
-  res.send(page(pagedata))
-}
+    res.send(page(pagedata, config.warningLevel))
+  }
--- src/routes/root/page.ts
+++ src/routes/root/page.ts
@@ -14,10 +14,15 @@ const COLORS = {
   error: '#e00',
 }
 
-export function page({levels, logs}: Args) {
+export function page({levels, logs}: Args, warningLevel: number) {
   const levelsHtml = `<h3>Levels</h3><table border=1>
   <tr><th>When</th><th>Value</th></tr>
-  ${levels.map(({value, when}) => `<tr><td>${when.toLocaleString()}</td><td>${value}</td></tr>`).join('')}
+  ${levels
+    .map(({value, when}) => {
+      const style = value >= warningLevel ? ` style="color:${COLORS.warn}"` : ''
+      return `<tr${style}><td>${when.toLocaleString()}</td><td>${value}</td></tr>`
+    })
+    .join('')}
 </table>`
   const logsHtml = `<h3>Logs</h3><table border=1>
   <tr><th>When</th><th>Severity</th><th>Message</th></tr>
