b11c9752797b0a6cc33eaa5b06d0dfb57bc87a9e
Phil Tsaryk <phil@tsaryk.com>
2025-09-13 18:18:28 +0200

    feat(db): return result from exec

--- src/db.ts
+++ src/db.ts
@@ -34,16 +34,18 @@ export function init(config: Config) {
   uri = config.db.uri
 }
 
-export async function exec<T extends Document>(
+export async function exec<T extends Document, R = unknown>(
   name: string,
-  operation: (collection: Collection<T>) => Promise<unknown>
-) {
+  operation: (collection: Collection<T>) => Promise<R>
+): Promise<R> {
+  let res: R
   const client = new MongoClient(uri)
   try {
     const db = client.db()
     const collection = db.collection<T>(name)
-    await operation(collection)
+    res = await operation(collection)
   } finally {
     await client.close()
   }
+  return res
 }
