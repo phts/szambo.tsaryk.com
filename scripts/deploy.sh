eb05683abaf3a0bb53db600382b971b909f7665e
Phil Tsaryk <phil@tsaryk.com>
2025-10-29 10:44:23 +0100

    chore: separate env files for deployment and for app

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -f .env ]; then
-  source .env
+if [ -f .env-deploy ]; then
+  source .env-deploy
 fi
 
 if [[ -z "${SSH_HOST}" || -z "${SSH_PATH}" || -z "${SSH_SCRIPT}" || -z "${SSH_USER}" ]]; then
